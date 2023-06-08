e58f96226d375329ac6a86b84971bc484d063a13
Kaan Hacıhaliloğlu <60143024+khdoex@users.noreply.github.com>
2023-06-08 20:28:09 +0300

    Added a new feature_extraction_pipeline.py file

---
+++ feature_extraction_pipeline.py
@@ -0,0 +1,24 @@
+if return_tensors:
+        return predictions
+    else:
+        return predictions.numpy()
+
+
+class FeatureExtractionPipeline(Pipeline):
+    def __init__(
+        self,
+        model: Union["PreTrainedModel", "TFPreTrainedModel"],
+        tokenizer: PreTrainedTokenizer,
+        modelcard: Optional[ModelCard] = None,
+        framework: Optional[str] = None,
+        args_parser: ArgumentHandler = None,
+        device: int = -1,
+    ):
+        super().__init__(
+            model=model,
+            tokenizer=tokenizer,
+            modelcard=modelcard,
+            framework=framework,
+            args_parser=args_parser,
+            device=device,
+        )
